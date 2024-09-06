df0ca6e363053bc6e79119792fdabf7b24611b88
Ronny Bergmann <git@ronnybergmann.net>
2024-09-06 09:06:07 +0200

    adapt make.jl to make sure IJulia is compiled/updated upfront

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,33 @@
 #
 #
 
+#!/usr/bin/env julia
+#
+#
+
+if "--help" ∈ ARGS
+    println(
+        """
+docs/make.jl
+
+Render the `Manopt.jl` documentation with optional arguments
+
+Arguments
+* `--help`              - print this help and exit without rendering the documentation
+* `--prettyurls`        – toggle the prettyurls part to true (which is otherwise only true on CI)
+* `--quarto`            – run the Quarto notebooks from the `tutorials/` folder before generating the documentation
+  this has to be run locally at least once for the `tutorials/*.md` files to exist that are included in
+  the documentation (see `--exclude-examples`) for the alternative.
+  If they are generated once they are cached accordingly.
+  Then you can spare time in the rendering by not passing this argument.
+  If quarto is not run, some tutorials are generated as empty files, since they
+  are referenced from within the documentation. These are currently
+  `Optimize.md` and `ImplementOwnManifold.md`.
+""",
+    )
+    exit(0)
+end
+
 #
 # (a) if docs is not the current active environment, switch to it
 # (from https://github.com/JuliaIO/HDF5.jl/pull/1020/) 
@@ -13,7 +40,7 @@ if Base.active_project() != joinpath(@__DIR__, "Project.toml")
     Pkg.instantiate()
 end
 
-# (b) Did someone say render? Then we render!
+# (b) Did someone say render?
 if "--quarto" ∈ ARGS
     using CondaPkg
     CondaPkg.withenv() do
@@ -21,12 +48,13 @@ if "--quarto" ∈ ARGS
         examples_folder = (@__DIR__) * "/../examples"
         # instantiate the tutorials environment if necessary
         Pkg.activate(examples_folder)
+        # For a breaking release -> also set the tutorials folder to the most recent version
         Pkg.develop(PackageSpec(; path=(@__DIR__) * "/../"))
         Pkg.resolve()
         Pkg.instantiate()
+        Pkg.build("IJulia") # build `IJulia` to the right version.
         Pkg.activate(@__DIR__) # but return to the docs one before
-        run(`quarto render $(examples_folder)`)
-        return nothing
+        return run(`quarto render $(examples_folder)`)
     end
 end
 
@@ -55,6 +83,8 @@ open(joinpath(generated_path, "contributing.md"), "w") do io
     end
 end
 
+examples_menu = ["Examples" => ["Cubic Hermite interpolation" => "examples/hermite.md"]]
+
 bib = CitationBibliography(joinpath(@__DIR__, "src", "references.bib"); style=:alpha)
 makedocs(;
     format=HTML(; mathengine=MathJax3(), prettyurls=get(ENV, "CI", nothing) == "true"),
@@ -62,6 +92,7 @@ makedocs(;
     modules=[ManifoldExamples],
     pages=[
         "Home" => "index.md",
+        [examples_menu]...,
         "Examples" => ["Cubic Hermite interpolation" => "examples/hermite.md"],
         "Contributing to ManifoldExamples.jl" => "contributing.md",
         "References" => "src/references.md",
